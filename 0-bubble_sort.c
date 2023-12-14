07e79a79cd9f4d38f60297f793036505f4508213
Savanna Davis <saavannaa.rose@gmail.com>
2023-12-14 09:08:08 -0800

    adding a check for if array is null or size is 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,6 +9,9 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
 
+	if (array == NULL || size == 0)
+		exit(EXIT_FAILURE);
+
 	for (i = 0; i < size - 1; i++)
 	{
 		for (j = 0; j < size - i - 1; j++)

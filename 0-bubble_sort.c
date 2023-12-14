5fdaa8de50fbb08aea4d3cc2a0722634ad0c63f9
Savanna Davis <saavannaa.rose@gmail.com>
2023-12-14 08:49:16 -0800

    added bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,27 @@
+#include "sort.h"
+/**
+ * bubble_sort - sorts based on the bubble sort method
+ * 
+ * @array: array to be sorted
+ * @size: size of the array
+ **/
+void bubble_sort(int *array, size_t size)
+{
+	int i, j;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+				swap(&array[j], &array[j + 1]);
+		}
+	}
+}
+
+void swap(int *a, int *b)
+{
+	int *temp = *a;
+	*a = *b;
+	*b = temp;
+}

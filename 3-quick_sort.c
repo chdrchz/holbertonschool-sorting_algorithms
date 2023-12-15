360e95a52d1d9f12ddb0991632e19a1211c63870
Savanna Davis <saavannaa.rose@gmail.com>
2023-12-15 10:25:07 -0800

    adding documentation

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,5 +1,11 @@
 #include "sort.h"
 #include "swap.c"
+/**
+ * quick_sort - sorts an array of ints in ascending order
+ *
+ * @array: array of ints
+ * @size: size of array
+ **/
 void quick_sort(int *array, size_t size)
 {
 	if (size > 0)
@@ -7,6 +13,14 @@ void quick_sort(int *array, size_t size)
 		quick_sort_recursive(array, 0, size - 1);
 	}
 }
+
+/**
+ * quick_sort_recursive - recursive function for quick sort algorithim
+ *
+ * @array: array of ints
+ * @low: lowest int
+ * @high: highest int
+ **/
 void quick_sort_recursive(int *array, int low, int high)
 {
 	int partitionIndex;
@@ -18,6 +32,15 @@ void quick_sort_recursive(int *array, int low, int high)
 		quick_sort_recursive(array, partitionIndex + 1, high);
 	}
 }
+
+/**
+ * partition - handles the pivot point for swapping spaces in the array
+ *
+ * @array: array of ints
+ * @low: lowest int
+ * @high: highest int
+ * Return: i + 1
+ **/
 int partition(int *array, int low, int high)
 {
 	int pivot = array[high];

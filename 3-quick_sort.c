c133779772b6bc244b2593b885936d8a89639800
Savanna Davis <saavannaa.rose@gmail.com>
2023-12-15 10:04:42 -0800

    adding quick sort

---
+++ 3-quick_sort.c
@@ -0,0 +1,36 @@
+#include "sort.h"
+#include "swap.c"
+void quick_sort(int *array, size_t size)
+{
+	if (size > 0)
+	{
+		quick_sort_recursive(array, 0, size - 1);
+	}
+}
+void quick_sort_recursive(int *array, int low, int high)
+{
+	int partitionIndex;
+
+	if (low < high)
+	{
+		partitionIndex = partition(array, low, high);
+		quick_sort_recursive(array, low, partitionIndex - 1);
+		quick_sort_recursive(array, partitionIndex + 1, high);
+	}
+}
+int partition(int *array, int low, int high)
+{
+	int pivot = array[high];
+	int i = low - 1, j = 0;
+
+	for (j = low; j <= high - 1; j++)
+	{
+		if (array[j] < pivot)
+		{
+			i++;
+			swap(&array[i], &array[j]);
+		}
+	}
+	swap(&array[i + 1], &array[high]);
+	return i + 1;
+}
--- sort.h
+++ sort.h
@@ -29,5 +29,8 @@ void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void quick_sort_recursive(int *array, int low, int high);
+int partition(int *array, int low, int high);
 
 #endif
